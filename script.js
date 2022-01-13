3e1d3a64022a6e75bfd872e27b8cf7ae8fbcd948
Lucas Cardoso de Castro <sky.lucas2013@gmail.com>
2022-01-12 23:32:57 -0300

    update form with jquery

--- script.js
+++ script.js
@@ -1,180 +1,262 @@
 jQuery(function ($) {
-  var table = $('.table')
-  var formQuest = $('#formQuest')
-  var valuesSelecteds = new Array()
-  var count = 0
+  var formQuest = $("#formQuest");
+  var valuesSelecteds = new Array();
+  $("#selectAll").hide();
 
   formQuest.validate({
     rules: {
       first: {
-        required: true
+        required: true,
       },
       last: {
-        required: true
+        required: true,
       },
       email: {
         required: true,
-        email: true
-      }
+        email: true,
+      },
     },
     messages: {
       first: {
-        required: 'Esse campo não pode ser vazio'
+        required: "Esse campo não pode ser vazio",
       },
       last: {
-        required: 'Esse campo não pode ser vazio'
+        required: "Esse campo não pode ser vazio",
       },
       email: {
-        required: 'Esse campo não pode ser vazio',
-        email: 'Insira um e-mail válido'
-      }
+        required: "Esse campo não pode ser vazio",
+        email: "Insira um e-mail válido",
+      },
     },
     submitHandler: function (form) {
-      var data = []
-      $('#formQuest :input[type=text], input[type=email]').each(function (i) {
-        var input = $(this)
-        var name = input.attr('name')
-        data[name] = input.val()
-      })
-      formQuest.trigger('reset')
-      $('#modalQuestion').modal('toggle')
-      createRow(data)
-    }
-  })
+      let issetEmail = issetUser($("#email").val());
+      if (issetEmail === true) {
+        alertify.alert("Email já cadastrado.").set({ title: "Atenção" });
+      } else {
+        var data = [];
+        $("#formQuest :input[type=text], input[type=email]").each(function (i) {
+          var input = $(this);
+          var name = input.attr("name");
+          data[name] = input.val();
+        });
+        formQuest.trigger("reset");
+        $("#modalUser").modal("toggle");
+        createRow(data);
+        showSelectAll();
+      }
+    },
+  });
 
-  $('#inputSearch').on('keyup', function () {
-    var value = $(this)
-      .val()
-      .toLowerCase()
-    $('.table .lineTable').filter(function () {
-      $(this).toggle(
-        $(this)
-          .text()
-          .toLowerCase()
-          .indexOf(value) > -1
-      )
-    })
-  })
+  $("#inputSearch").on("keyup", function () {
+    var value = $(this).val().toLowerCase();
+    $(".table .lineTable").filter(function () {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
+    });
+  });
 
-  $(document).on('click', '.deleteRow', function () {
-    var deleteButton = $(this)
+  $(document).on("click", ".deleteRow", function () {
+    var deleteButton = $(this);
 
     alertify
-      .confirm('Tem certeza que deseja excluir este usuário?', function () {
-        deleteButton.closest('tr').remove()
-        alertify.success('Deletado com sucesso!')
-        updateCounterRows()
+      .confirm("Tem certeza que deseja excluir este usuário?", function () {
+        deleteButton.closest("tr").remove();
+        alertify.success("Deletado com sucesso!");
+        updateCounterRows();
+        showSelectAll();
       })
-      .set({ title: 'Atenção' })
-      .set({ labels: { ok: 'Confirmar', cancel: 'Cancelar' } })
-  })
+      .set({ title: "Atenção" })
+      .set({ labels: { ok: "Confirmar", cancel: "Cancelar" } });
+  });
 
-  $(document).on('click', '#deleteSelecteds', function () {
+  $(document).on("click", "#deleteSelecteds", function () {
     alertify
-      .confirm('Tem certeza que deseja excluir estes usuários?', function () {
+      .confirm("Tem certeza que deseja excluir estes usuários?", function () {
         for (let i in valuesSelecteds) {
-          $('.selectedRow').each(function () {
+          $(".selectedRow").each(function () {
             if ($(this).val() === valuesSelecteds[i]) {
               valuesSelecteds.splice(
                 valuesSelecteds.indexOf(valuesSelecteds[i]),
                 1
-              )
-              $(this)
-                .closest('tr')
-                .remove()
+              );
+              $(this).closest("tr").remove();
 
-              removeButtonDeleteAll()
-              updateCounterRows()
+              removeButtonDeleteAll();
+              updateCounterRows();
+              showSelectAll();
             }
-          })
+          });
         }
-        alertify.success('Deletados com sucesso!')
+        alertify.success("Usuários deletados com sucesso!");
       })
-      .set({ title: 'Atenção' })
-      .set({ labels: { ok: 'Confirmar', cancel: 'Cancelar' } })
-  })
-
-  $(document).on('change', '.selectedRow', function () {
-    if ($(this).is(':checked')) {
-      valuesSelecteds.push($(this).val())
-    } else {
-      valuesSelecteds.splice(valuesSelecteds.indexOf($(this).val()), 1)
-    }
-
-    removeButtonDeleteAll()
-  })
-
-  function createRow (data) {
-    var countRow = $('.counter').before().length + 1
-
-    if (countRow <= 2) {
-      table.append(
-        `
-             <tr class="lineTable">
-                  <th><input type="checkbox" value="${countRow}" class="selectedRow" name="selectedRow" /></th>
-                  <th class="counter"></th>
-                  <td>${data.first}</td>
-                  <td>${data.last}</td>
-                  <td>${data.email}</td>
-                  <td><button type="button" class="btn btn-outline-info btn-sm editRow">Edit</button></td>
-                  <td><button type="button" class="btn btn-outline-danger btn-sm deleteRow">Excluir</button></td>
-             </tr>
-          `
-      )
-
-      var notification = alertify.notify(
-        'Usuário criado com sucesso',
-        'success',
-        5
-      )
-    } else {
-      alertify
-        .alert('Não é possível criar mais de 3 usuários!')
-        .set({ title: 'Atenção' })
-    }
-
-    updateCounterRows()
-  }
-
-  $('.editRow').on('click', function() {
-
+      .set({ title: "Atenção" })
+      .set({ labels: { ok: "Confirmar", cancel: "Cancelar" } });
   });
 
-  function removeButtonDeleteAll () {
-    if (valuesSelecteds.length > 0) {
-      $('#deleteSelecteds').remove()
-      $('.card-header').prepend(
-        `
-          <button class="btn btn-danger" id="deleteSelecteds">Deletar x${valuesSelecteds.length}</button>
-          `
-      )
+  $(document).on("change", ".selectedRow", function () {
+    if ($(this).is(":checked")) {
+      valuesSelecteds.push($(this).val());
     } else {
-      $('#deleteSelecteds').remove()
+      valuesSelecteds.splice(valuesSelecteds.indexOf($(this).val()), 1);
+    }
+
+    removeButtonDeleteAll();
+  });
+
+  $(document).on("click", ".editRow", function () {
+    let buttonClick = $(this);
+    let baseColumn = $(this).closest(".lineTable");
+
+    let firstInput = baseColumn.find('[class*="firstColumn"]');
+    let lastInput = baseColumn.find('[class*="lastColumn"]');
+    let emailInput = baseColumn.find('[class*="emailColumn"]');
+
+    if (buttonClick.text() === "Editar") {
+      buttonClick.text("Concluir");
+
+      $(".editRow").each(function () {
+        if ($(this).text() === "Editar") {
+          $(this).prop("disabled", true);
+        }
+      });
+
+      firstInput.html(
+        `<input type="text" class="editFirst edit-input" name="editFirst" value="${firstInput.text()}">`
+      );
+      lastInput.html(
+        `<input type="text" class="editLast edit-input" name="editLast" value="${lastInput.text()}">`
+      );
+      emailInput.html(
+        `<input type="email" class="editEmail edit-input" name="editEmail" value="${emailInput.text()}">`
+      );
+    } else {
+      if (
+        $(".editFirst").val().length > 0 &&
+        $(".editLast").val().length > 0 &&
+        $(".editEmail").val().length > 0
+      ) {
+        buttonClick.text("Editar");
+
+        $(".editRow").each(function () {
+          if (buttonClick.text() === "Editar") {
+            $(this).prop("disabled", false);
+          }
+        });
+
+        firstInput.html(`${$(".editFirst").val()}`);
+        lastInput.html(`${$(".editLast").val()}`);
+        emailInput.html(`${$(".editEmail").val()}`);
+      } else {
+        alertify.error("Preencha todos os campos.");
+      }
+    }
+  });
+
+  $("#selectAll").on("change", function () {
+    let check = $(this);
+
+    if (check.is(":checked")) {
+      $(".selectedRow").prop("checked", true);
+      $(".selectedRow").each(function () {
+        valuesSelecteds.push($(this).val());
+      });
+    } else {
+      $(".selectedRow").prop("checked", false);
+      $(".selectedRow").each(function () {
+        valuesSelecteds.splice(valuesSelecteds.indexOf($(this).val()), 1);
+      });
+    }
+
+    removeButtonDeleteAll();
+  });
+
+  function createRow(data) {
+    var countRow = $(".counter").before().length + 1;
+
+    if (countRow <= 3) {
+      $(".table > tbody").append(
+        `
+             <tr class="lineTable">
+                  <td><input type="checkbox" value="${countRow}" class="selectedRow" name="selectedRow" /></td>
+                  <td class="counter"></td>
+                  <td class="firstColumn">${data.first}</td>
+                  <td class="lastColumn">${data.last}</td>
+                  <td class="emailColumn">${data.email}</td>
+                  <td width="90"><button type="button" class="editRow">Editar</button></td>
+                  <td width="30"><button type="button" class="deleteRow"><i class="fas fa-trash"></i></button></td>
+             </tr>
+          `
+      );
+
+      var notification = alertify.notify(
+        "Usuário criado com sucesso",
+        "success",
+        5
+      );
+    } else {
+      alertify
+        .alert("Não é possível criar mais de 3 usuários!")
+        .set({ title: "Atenção" });
+    }
+
+    updateCounterRows();
+  }
+
+  function removeButtonDeleteAll() {
+    if (valuesSelecteds.length > 0) {
+      $("#deleteSelecteds").remove();
+      $(".card-header-default").prepend(
+        `
+          <button class="button-danger-default" id="deleteSelecteds">Deletar x${valuesSelecteds.length}</button>
+          `
+      );
+    } else {
+      $("#selectAll").prop("checked", false);
+      $("#deleteSelecteds").remove();
     }
   }
 
-  function updateCounterRows () {
-    var countRow = $('.counter').before().length
-    $('.counterRowsTable').html(`Contador: ${countRow}`)
+  function updateCounterRows() {
+    var countRow = $(".counter").before().length;
+    $(".counterRowsTable").html(`Número de linhas - ${countRow}`);
   }
 
-  updateCounterRows()
+  function showSelectAll() {
+    if ($(`.lineTable`).length > 0) {
+      $("#selectAll").show();
+      return true;
+    }
+    $("#selectAll").hide();
 
-  function getAllUsers () {
+    return false;
+  }
+
+  function issetUser(email) {
+    let bool = false;
+
+    if ($(`.emailColumn:contains(${email})`).length > 0) {
+      bool = true;
+    }
+
+    return bool;
+  }
+
+  function getAllUsers() {
     $.ajax({
-      type: 'GET',
-      url: 'http://localhost:3000/users',
+      type: "GET",
+      url: "http://localhost:3000/users",
       success: function (data) {
-        console.log(data)
+        console.log(data);
 
         for (let i in data) {
-          createRow(data[i])
+          createRow(data[i]);
         }
       },
       error: function (data) {
-        console.log('An error occurred.')
-        console.log(data)
-      }
-    })
+        console.log("An error occurred.");
+        console.log(data);
+      },
+    });
   }
-})
+
+  updateCounterRows();
+});
